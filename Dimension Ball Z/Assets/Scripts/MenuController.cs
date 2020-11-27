3d6899e16ff6f0b40b3929f90db5fca5f693bc9f
emilorn <emmikall95@gmail.com>
2020-11-27 18:29:54 +0000

    Managed sprites

---
+++ Dimension Ball Z/Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    
+
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private void Start()
+    {
+        
+    }
+
+
+}
+
