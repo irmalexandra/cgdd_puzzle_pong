20784d8170bd4faba4c2930c24612551141d3fba
emilorn <emmikall95@gmail.com>
2020-11-27 22:29:06 +0000

    Emils pong upload

---
+++ Dimension Ball Z/Assets/Scripts/emilsTests/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D body;
+    public Vector2 direction;
+    public float impulse;
+    void Start()
+    {
+        body.velocity = direction.normalized * impulse;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
