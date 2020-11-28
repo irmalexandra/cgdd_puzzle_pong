e0fab2f81aedb0770a45611cf2d2de0107da5b88
Loki Hopkins <nyms.hopkins@gmail.com>
2020-11-28 11:50:39 +0000

    Created a Level Complete portal.

---
+++ Dimension Ball Z/Assets/Scripts/WinPortalController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPortalController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision){
+
+        Destroy(collision.collider.gameObject);
+        Application.LoadLevel("EndLevel");
+    }
+}
