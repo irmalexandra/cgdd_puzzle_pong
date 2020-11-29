971eceebe22f4a00f3c7d3ac2314cb0b6be2a787
emilorn <emmikall95@gmail.com>
2020-11-29 17:32:10 +0000

    Button + door prefab in emil folder

---
+++ Dimension Ball Z/Assets/Scripts/emilsTests/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "ButtonFace")
+        {
+            door.gameObject.GetComponent<DoorController>().activateDoor();
+        }
+    }
+}
---
+++ Dimension Ball Z/Assets/Scripts/emilsTests/DoorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public void activateDoor()
+    {
+        JointMotor2D direction = GetComponent<HingeJoint2D>().motor;
+        direction.motorSpeed *= -1;
+        GetComponent<HingeJoint2D>().motor = direction;
+    }
+}
