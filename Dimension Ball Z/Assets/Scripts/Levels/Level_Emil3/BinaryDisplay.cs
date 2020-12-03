6a471e7a1e820cf02e8811d25ee2113e8c2ffd7b
emilorn <emmikall95@gmail.com>
2020-12-03 15:17:58 +0000

    emil level 3 and fixed barrel physics

---
+++ Dimension Ball Z/Assets/Scripts/Levels/Level_Emil3/BinaryDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BinaryDisplay : MonoBehaviour
+{
+
+    public static BinaryDisplay Instance;
+
+    public TMPro.TextMeshProUGUI currentBinary;
+    public TMPro.TextMeshProUGUI currentInt;
+    public TMPro.TextMeshProUGUI requiredInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        requiredInt.text = Random.Range(3, 8).ToString();
+        Instance = this;
+    }
+
+    public void Signal(int input)
+    {
+        currentBinary.text += input;
+        
+    }
+}
---
+++ Dimension Ball Z/Assets/Scripts/Levels/Level_Emil3/BinaryInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int input;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "ButtonFace")
+        {
+            BinaryDisplay.Instance.Signal(input);
+        }
+    }
+}
