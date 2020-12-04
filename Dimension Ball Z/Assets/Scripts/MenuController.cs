a236c3a968fe6702ec5343802b0f00453c8e4d72
R1klus <rikhardur.fridgeirsson@gmail.com>
2020-12-04 10:59:33 +0000

    added tutorial scene

--- Dimension Ball Z/Assets/Scripts/MenuController.cs
+++ Dimension Ball Z/Assets/Scripts/MenuController.cs
@@ -4,7 +4,10 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
 
-    
+    public void ShowTutorialI()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
 
     // Start is called before the first frame update
     public void PlayGame()
