4f2fd4882d0a5535840e55f1eacd0d0d44366189
Arnarps <arnarps@gmail.com>
2020-12-06 18:20:44 +0000

    script to reverse gravity on contact with the ball

---
+++ Dimension Ball Z/Assets/Scripts/ReverseGravity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseGravity : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Ball")) return;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+
+
+
+    }
+}
