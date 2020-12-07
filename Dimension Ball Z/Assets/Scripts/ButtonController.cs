9e5d418d43ef0a193f6ae8dbd457953c1732bf3e
emilorn <emmikall95@gmail.com>
2020-12-07 19:24:39 +0000

    Changed button prefab

--- Dimension Ball Z/Assets/Scripts/ButtonController.cs
+++ Dimension Ball Z/Assets/Scripts/ButtonController.cs
@@ -1,17 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ButtonController : MonoBehaviour
 {
     public GameObject[] toActivate;
     public bool hold;
     private bool _open;
+    public GameObject buttonFace;
+    private Color _buttonFaceInvertedColor;
+    private Color _buttonFaceOriginalColor;
+    private Color _buttonFaceLightOriginalColor;
+    private Color _buttonFaceLightInvertedColor;
 
+    private void Start()
+    {
+        _buttonFaceOriginalColor = buttonFace.GetComponent<SpriteRenderer>().color;
+        _buttonFaceInvertedColor = Invertcolor(_buttonFaceOriginalColor);
+        _buttonFaceLightOriginalColor = buttonFace.GetComponentInChildren<Light2D>().color;
+        _buttonFaceLightInvertedColor = Invertcolor(_buttonFaceLightOriginalColor);
+    }
+
+
+    private Color Invertcolor(Color originalColor)
+    {
+        
+        Color newColor = originalColor;
+        Color.RGBToHSV(newColor, out float H, out float S, out float V);
+        float negativeH = (H + 0.5f) % 1f;
+        Color negativeColor = Color.HSVToRGB(negativeH, S, V);
+        return negativeColor;
+    }
+
+    private void invertButtonFaceColors(Collision2D other)
+    {
+        if (buttonFace.GetComponent<SpriteRenderer>().color == _buttonFaceInvertedColor)
+        {
+            buttonFace.GetComponent<SpriteRenderer>().color = _buttonFaceOriginalColor;
+            buttonFace.GetComponentInChildren<Light2D>().color = _buttonFaceLightOriginalColor;
+        }
+        else
+        {
+            buttonFace.gameObject.GetComponent<SpriteRenderer>().color = _buttonFaceInvertedColor;
+            buttonFace.GetComponentInChildren<Light2D>().color = _buttonFaceLightInvertedColor;
+        }
+    }
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         
+
         if (other.gameObject.name == "ButtonFace")
         {
-            
+            invertButtonFaceColors(other);
             foreach (var interactableObject in toActivate)
             {
                 interactableObject.GetComponent<InteractablesController>().Signal();
@@ -24,6 +65,7 @@ public class ButtonController : MonoBehaviour
     {
         if (hold && other.gameObject.name == "ButtonFace")
         {
+            invertButtonFaceColors(other);
             foreach (var interactableObject in toActivate)
             {
                 interactableObject.GetComponent<InteractablesController>().Signal();
