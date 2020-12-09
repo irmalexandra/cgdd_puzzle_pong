3950097502c312c3390c39716f9f6a0eeb0b6ff2
Emil Örn <emmikall95@gmail.com>
2020-12-09 20:36:45 +0000

    updates to spawner and emil lvl 4

--- Dimension Ball Z/Assets/Scripts/SpawnerController.cs
+++ Dimension Ball Z/Assets/Scripts/SpawnerController.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
     public GameObject toSpawn;
-    
+    public int allowedAmount;
+    private List<GameObject> _currentSpawns;
+
+
+    private void Start()
+    {
+        _currentSpawns = new List<GameObject>();
+    }
 
     public void Spawn()
     {
-        Instantiate(toSpawn, transform);
+        GameObject spawn = Instantiate(toSpawn, transform);
+        _currentSpawns.Add(spawn);
+        if (_currentSpawns.Count > allowedAmount)
+        {
+            Destroy(_currentSpawns[0]);
+            _currentSpawns.RemoveAt(0);
+        }
     }
 }
