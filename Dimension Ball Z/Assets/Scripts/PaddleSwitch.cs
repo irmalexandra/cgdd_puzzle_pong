79f33be42117c5d5f5c8d3688001df30bf9483e5
Ríkharður Friðgeirsson <rikhardur.fridgeirsson@gmail.com>
2020-12-09 16:09:20 +0000

    fixed paddle switch bug and flashing

--- Dimension Ball Z/Assets/Scripts/PaddleSwitch.cs
+++ Dimension Ball Z/Assets/Scripts/PaddleSwitch.cs
@@ -2,7 +2,7 @@
 
 public class PaddleSwitch : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
