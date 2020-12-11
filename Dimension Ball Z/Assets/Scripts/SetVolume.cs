89b5fd54fe1c25bdcdfcf3f702224d4b7835904f
nyms <nyms.hopkins@gmail.com>
2020-12-11 15:21:49 +0000

    Got volume slider working with player prefs

--- Dimension Ball Z/Assets/Scripts/SetVolume.cs
+++ Dimension Ball Z/Assets/Scripts/SetVolume.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -6,11 +7,19 @@ using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
-
     public AudioMixer mixer;
+    public Slider slider;
+
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+    }
 
     public void SetLevel(float sliderValue)
     {
+        
         mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+        PlayerPrefs.Save();
     }
 }
